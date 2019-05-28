ba86f742c6f1629d6d394cbfff72765342f11073
SEJ <sej@needrent.dk>
2019-05-28 10:17:24 +0200

    Index page, Base CSS

--- js/frontpage.js
+++ js/frontpage.js
@@ -1,1 +1,29 @@
 console.log("We are connected");
+
+const burgerMenuBtn = document.querySelector("#burger-menu");
+const burgerMenuSpan = document.querySelectorAll("#burger-menu span");
+const menuCon = document.querySelector("#menu");
+const menuBtns = document.querySelectorAll("#menu a");
+
+burgerMenuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+    burgerMenuSpan[1].classList.toggle("middel");
+
+    setTimeout(crossIn, 0);
+
+    function crossIn() {
+    burgerMenuSpan[0].classList.toggle("topX");
+
+    burgerMenuSpan[2].classList.toggle("bottomX");
+    setTimeout(menuIn, 0);
+
+    function menuIn() {
+        menuCon.classList.toggle("opened");
+    }
+}
+}
+menuBtns.onclick = function() {
+   console.log("clicked");
+}
+console.log(menuBtns);
