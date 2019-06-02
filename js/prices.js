2b575acf5ecea5743ecbee9bc7ae084f86d84977
SEJ <sej@needrent.dk>
2019-06-02 19:38:07 +0200

    Making the prices dynamic

---
+++ js/prices.js
@@ -0,0 +1,23 @@
+const burgerMenuBtn = document.querySelector("#burger-menu");
+const burgerMenuSpan = document.querySelectorAll("#burger-menu span");
+const menuCon = document.querySelector("#menu");
+const menuBtns = document.querySelectorAll("#menu a");
+
+burgerMenuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+    burgerMenuSpan[1].classList.toggle("middel");
+
+    setTimeout(crossIn, 0);
+
+    function crossIn() {
+    burgerMenuSpan[0].classList.toggle("topX");
+
+    burgerMenuSpan[2].classList.toggle("bottomX");
+    setTimeout(menuIn, 0);
+
+    function menuIn() {
+        menuCon.classList.toggle("opened");
+    }
+}
+}
